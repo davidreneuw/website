d28c921541ed2d2e67825c6fd1ee89f03a247e8b
davidreneuw <davrene11@gmail.com>
2023-05-11 22:46:08 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <html>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+        ></link>
       </header>
-    </div>
+      <div className="content">
+        <div className="container">
+          <ul className="nav nav-pills flex-column">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                test
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                test2
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                test3
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="container">test</div>
+      </div>
+    </html>
   );
 }
 
